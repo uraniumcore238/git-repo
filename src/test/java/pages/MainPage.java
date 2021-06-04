954f3d92e807b89f824aa242d949bf9ea140ae96
uraniumcore238@gmail.com <Qwe2asd9>
2021-06-04 10:42:32 +0500

    added sending message test

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -6,8 +6,7 @@ import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 public class MainPage {
@@ -17,7 +16,14 @@ public class MainPage {
             inputPasswordBtn = $("button[data-testid='enter-password']"),
             passwordInputField = $("input[name='password']"),
             loginToMailBtn = $("button[data-testid='login-to-mail']"),
-            inboxBtn = $("a[href='/inbox/']");
+            inboxBtn = $("a[href='/inbox/']"),
+            sendMessageBtn = $("a[href='/compose/']"),
+            inputEmailField = $("div[class='contacts--1ofjA'] input"),
+            inputThemeField = $("input[name='Subject']"),
+            inputMessageField = $("div[contenteditable ='true']"),
+            sendLetterBtn = $("span[title='Отправить']"),
+            loginBtn = $(""),
+            exitBtn = $x("//div[.='Выйти']");
 
     public void OpenMainPage() {
         step("Open main page", () -> open(""));
@@ -34,6 +40,23 @@ public class MainPage {
         passwordInputField.setValue(password);
         loginToMailBtn.click();
         inboxBtn.shouldBe(visible);
+
+    }
+
+    @Step("Send test message")
+    public void sendTestMessage(String receiverEmail, String theme, String message) {
+        sendMessageBtn.click();
+        inputEmailField.sendKeys(receiverEmail);
+        inputThemeField.sendKeys(theme);
+        inputMessageField.sendKeys(message);
+        sendLetterBtn.click();
+    }
+
+    @Step("Log out")
+    public void logOut() {
+        loginBtn.click();
+
+
     }
 
 
--- src/test/java/tests/CheckTheMainPageTest.java
+++ src/test/java/tests/CheckTheMainPageTest.java
@@ -5,11 +5,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.MainPage;
-
+import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
 @Tag("web")
-public class CheckTheMainPageTest extends TestBase {
+public class CheckTheMainPageTest extends TestBase{
 
     MainPage mainPage = new MainPage();
 
@@ -23,6 +23,12 @@ public class CheckTheMainPageTest extends TestBase {
                 mainPage.login(ConfigHelper.getWebSenderLogin(),
                         ConfigHelper.getWebSenderPassword());
             });
+
+        step("Send test message", () -> {
+            mainPage. sendTestMessage("receiver-test-qa@mail.ru", "Тест", "Тест отправки");
+        });
+
+
 
     }
 
